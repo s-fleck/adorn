0ab0389826be089c2e27b660bfa0af01c0445b2b
Stefan Fleck <stefan.b.fleck@gmail.com>
2017-07-01 11:09:15 +0200

    added functions to copy/paste data.frames from the windows clipboard

---
+++ R/clipboard.R
@@ -0,0 +1,94 @@
+
+#' Read Tab-separted Tables from Windows Clipboard
+#'
+#' `read_clipboard_tab()` is a simple wrapper for `read.table()`, with presets
+#' set for tab separated table cells (such as cells copied from Excel or other
+#' spreadhseet software)
+#'
+#' @param ... passed on to [read_table()]
+#' @inheritParams utils::read.table
+#'
+#' @return a data.frame
+#' @family windows clipboard utils
+#' @export
+#' @md
+#'
+read_clipboard_tab <- function(header = FALSE, ...){
+  assert_windows_os()
+
+  read.table(file = "clipboard", sep = "\t", header = header, ...)
+}
+
+
+
+
+#' `paste_clipboard()` pastes tab-separted tables from windows clipboard to the
+#'   variable `adorn_clip` in the global environment
+#'
+#' @rdname read_clipboard_tab
+#' @export
+#' @md
+#'
+paste_clipboard <- function(header = FALSE, ...){
+  dat <- read_clipboard_tab(header  = header, ...)
+  assign('adorn_clip', dat, envir = globalenv())
+  invisible(dat)
+}
+
+
+
+
+#' Write an R Object to the Windows Clipboard
+#'
+#' @param x any R object
+#'
+#' @section Side Effects:
+#'   Write to the Windows Clipboard
+#'
+#' @family windows clipboard utils
+#' @return `x` (invisibly)
+#' @md
+#' @export
+#'
+write_clipboard <- function(x, ...){
+  assert_windows_os()
+  UseMethod('write_clipboard')
+}
+
+
+
+
+#' Write a Data Frame to the Windows Clipboard
+#'
+#' @param x a data.frame
+#' @param sep a scalar character. Defaults to `\\t` (Tab) for compatibilty with
+#'   Excel or similar software
+#' @param ... further arguments passed on to [write.table()]
+#' @inheritParams utils::write.table
+#'
+#' @section Side Effects:
+#'   Write a Data Frame to the Windows Clipboard
+#'
+#' @family windows clipboard utils
+#' @inherit write_clipboard return
+#' @export
+#' @md
+write_clipboard.data.frame <- function(
+  x,
+  row.names=FALSE,
+  sep="\t",
+  ...
+){
+  write.table(x, "clipboard", row.names = row.names, sep = "\t", ...)
+  invisible(x)
+}
+
+
+
+
+assert_windows_os <- function(){
+  os <- Sys.info()['sysname']
+  if(!identical(os, 'Windows')){
+    stop('Reading and writing from clipboard is only supported under Windows')
+  }
+}
